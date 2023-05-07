a476cc6cfb8a6cbdb2bad557eb7dee2b5f10058e
Elgene Yeo <elgeneyeo@hotmail.com>
2023-05-06 20:49:46 -0400

    Testing_commit: for testing chatbot API

--- ai_chatbot/langllamaSetup.py
+++
@@ -1,1 +0,0 @@
-from .openaiSetup import ChatOpenAI
--- ai_chatbot/shared_keys.py
+++ ai_chatbot/shared_keys.py
@@ -2,7 +2,7 @@ import os
 
 OPENAI_CHAT_MODEL = os.environ.get("OPENAI_CHAT_MODEL", "gpt-3.5-turbo")
 OPENAI_EMBEDDING_MODEL = os.environ.get("OPENAI_EMBEDDING_MODEL", "text-embedding-ada-002")
-TEMPERATURE = os.environ.get("TEMPERATURE", "0.5")
+TEMPERATURE = os.environ.get("TEMPERATURE", "0")
 CHUNK_SIZE = os.environ.get("CHUNK_SIZE", "256")
 CHUNK_OVERLAP = os.environ.get("CHUNK_OVERLAP", "20")
 MAX_TOKENS = os.environ.get("MAX_TOKENS", "512")
